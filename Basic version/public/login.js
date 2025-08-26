1b1df49ecb1e039f3d6a7fbae040451a83b14ca7
20058340 <barath>
2025-08-26 13:36:38 +0100

    updated

--- Basic version/public/login.js
+++ Basic version/public/login.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById('password').value;
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:4000/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
